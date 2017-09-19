4b1b40c0dfc7530bb5cae4ad1095032c9f195b60
queenskid <andres.ibarra0716@gmail.com>
2017-09-19 16:30:12 -0700

    file update with CSS additions and JS questions done

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 'use strict';
 
-alert('BOO!!!');
+alert('Who Goes There');
 
-//var userName = prompt('Thanks for stopping by my About Me page!, What is your name?');
+var userName = prompt('While you are here, What is your name?');
 
-//alert('Hi, ' + userName + ' nice to meet you. See if you can get the right answers to the following questions about me');
+alert('Hello ' + userName + ',' + ' nice to meet you. Let\'s play a little guessing game. Get them right to learn a little more about me. yes or no asnwers will get you there');
 
 // Queation 1
 
-var answer1 = prompt('Do I have 4 cats?').toLowerCase();
+var answer1 = prompt('Is the earth round?').toLowerCase();
 
-if (answer1 === 'n' || answer1 === 'no') {
+if (answer1 === 'y' || answer1 === 'yes') {
   // Tell user they got the correct answer
-  alert('You are correct');
+  alert('You got it!');
 } else {
   // Tell user they got the wrong answer
-  alert('You are incorrect');
+  alert('No. No. No.');
+}
+
+var answer2 = prompt('Do you think I smoke?').toLowerCase();
+
+if (answer2 === 'n' || answer2 === 'no'){
+  alert('Nice Guess ' + userName + '.');
+} else {
+  alert('WRONG');
+}
+
+var answer3 = prompt('Will Jon Snow win the Game of thrones?').toLowerCase();
+
+if (answer3 === 'y' || answer3 === 'yes'){
+  alert('I also have the same feeling ' + userName + '.' );
+} else {
+  alert('2 Thumbs Down for you ' + userName + '.');
+}
+
+var answer4 = prompt('Do you believe I will make an Awesome JavaScript Developer?').toLowerCase();
+
+if (answer4 === 'y' || answer4 === 'yes'){
+  alert('I hope you are right, I am trying my best to become one');
+} else {
+  alert('Oh, I was starting to like you ' + userName + '.');
+}
+
+var answer5 = prompt('last question ' + userName + ',' + ' can we be friends?').toLowerCase();
+
+if (answer5 === 'y' || answer5 === 'yes'){
+  alert('Sweet Beans ' + userName + '.');
+} else {
+  alert('I didnt want to be your friend anyways ' + userName + '.');
 }
